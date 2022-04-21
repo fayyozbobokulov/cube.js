9eac70f0593581dec6ba4758969b4dc14652dcad
George <g.andronchik@gmail.com>
2022-04-21 20:35:17 +0800

    chore(cubesql): get_expr query support (in progress)

--- rust/cubesql/cubesql/src/compile/rewrite/converter.rs
+++ rust/cubesql/cubesql/src/compile/rewrite/converter.rs
@@ -58,6 +58,7 @@ use cubeclient::models::{
 };
 use datafusion::arrow::datatypes::{DataType, TimeUnit};
 use datafusion::catalog::TableReference;
+use datafusion::logical_plan::Union;
 use datafusion::logical_plan::plan::Extension;
 use datafusion::logical_plan::plan::Filter;
 use datafusion::logical_plan::plan::Join;
@@ -1299,6 +1300,44 @@ impl LanguageToLogicalPlanConverter {
                 };
 
                 LogicalPlan::Extension(Extension { node })
+            }
+            LogicalPlanLanguage::Union(params) => {
+                // inputs,schema, alias
+
+                let inputs = match_list_node_ids!(node_by_id, params[0], UnionInputs)
+                    .into_iter()
+                    .map(|n| self.to_logical_plan(n))
+                    .collect::<Result<Vec<_>, _>>()?;
+
+
+                let schema = inputs.iter().fold(DFSchema::empty(), |a, b| {
+                    let mut res = a;
+                    res.merge(b.schema());
+                    res
+                });
+
+                // let schema = Arc::new(left.schema().join(right.schema())?);
+                
+                // let inputs = add_plan_list_node!(self, node.inputs, UnionInputs);
+                
+                
+
+                let alias = match_data_node!(node_by_id, params[1], UnionAlias);
+                
+                LogicalPlan::Union(Union {
+                    inputs,
+                    schema: Arc::new(schema),
+                    alias,
+                })
+
+                // let produce_one_row =
+                //     match_data_node!(node_by_id, params[0], EmptyRelationProduceOneRow);
+
+                // // TODO
+                // LogicalPlan::EmptyRelation(EmptyRelation {
+                //     produce_one_row,
+                //     schema: Arc::new(DFSchema::empty()),
+                // })
             }
             x => panic!("Unexpected logical plan node: {:?}", x),
         })
